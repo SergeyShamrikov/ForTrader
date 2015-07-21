64912e9f7601230798f92ee0dbc0dc1a3f45bd30
Сергей <sergey.shamrikov@gmail.com>
2015-07-22 01:12:18 +0300

    респонсив страницы inner

--- js/script.plugins.js
+++ js/script.plugins.js
@@ -23,13 +23,6 @@
 							paginationSpeed : 400,
 							singleItem:true
 
-							// "singleItem:true" is a shortcut for:
-							// items : 1, 
-							// itemsDesktop : false,
-							// itemsDesktopSmall : false,
-							// itemsTablet: false,
-							// itemsMobile : false
-
 						});
 
 					}
